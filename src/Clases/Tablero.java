b435db0cd421ba44ad87a7b49a98113cabb8d752
Ignacio <ignacioKomar@gmail.com>
2017-11-11 12:58:35 -0300

    commit prueba

---
+++ src/Clases/Tablero.java
@@ -0,0 +1,37 @@
+package Clases;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Tablero {
+	private HashMap<Jugador,Integer> posicionesJugadores;
+	private static final int CANTIDAD_CASILLAS = 20;
+
+	private int ajustarPosicionAlRangoDelTablero(int posicion) {
+		while(posicion > CANTIDAD_CASILLAS - 1) { 
+			posicion -= CANTIDAD_CASILLAS;
+		}
+		while(posicion < 0) {
+			posicion += CANTIDAD_CASILLAS;
+		}
+		return posicion;
+	}
+	
+	public Tablero(ArrayList<Jugador> jugadores) {
+		posicionesJugadores = new HashMap<Jugador,Integer>();
+		for(Jugador jugador : jugadores){
+			posicionesJugadores.put(jugador, 0);
+		}
+	}
+
+	public int getPosicion(Jugador jugador1) {
+		return posicionesJugadores.get(jugador1);
+	}
+
+	public void mover(Jugador jugador, int pasos) {
+		int nuevaPosicion = posicionesJugadores.get(jugador) + pasos;
+		nuevaPosicion = this.ajustarPosicionAlRangoDelTablero(nuevaPosicion);
+		posicionesJugadores.put(jugador, nuevaPosicion);
+	}
+
+}
---
+++ test/TestClases/TableroTest.java
@@ -0,0 +1,68 @@
+package TestClases;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Clases.Jugador;
+import Clases.Tablero;
+
+public class TableroTest {
+
+	@Test
+	public void test01AlCrearTableroCon3JugadoresEstosEmpiezanEnLaPosicion0() {
+		Jugador jugador1 = new Jugador();
+		Jugador jugador2 = new Jugador();
+		Jugador jugador3 = new Jugador();
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador1);
+		jugadores.add(jugador2);
+		jugadores.add(jugador3);
+		Tablero tablero = new Tablero(jugadores);
+		Assert.assertEquals(0, tablero.getPosicion(jugador1));
+		Assert.assertEquals(0, tablero.getPosicion(jugador2));
+		Assert.assertEquals(0, tablero.getPosicion(jugador3));
+	}
+	
+	@Test
+	public void test02UnJugadorEnSalidaTerminaEnLaPosicionCorrectaAlAvanzar7Lugares(){
+		Jugador jugador = new Jugador();
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador);
+		Tablero tablero = new Tablero(jugadores);
+		tablero.mover(jugador,7);
+		Assert.assertEquals(7, tablero.getPosicion(jugador));
+	}
+	
+	@Test
+	public void test03UnJugadorEnSalidaTerminaEnLaPosicionCorrectaAlRetroceder3Lugares(){
+		Jugador jugador = new Jugador();
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador);
+		Tablero tablero = new Tablero(jugadores);
+		tablero.mover(jugador,-3);
+		Assert.assertEquals(17, tablero.getPosicion(jugador));
+	}
+	
+	@Test
+	public void test04UnJugadorEnSalidaTerminaEnLaPosicionCorrectaAlAvanzar70Lugares(){
+		Jugador jugador = new Jugador();
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador);
+		Tablero tablero = new Tablero(jugadores);
+		tablero.mover(jugador,70);
+		Assert.assertEquals(10, tablero.getPosicion(jugador));
+	}
+
+	@Test
+	public void test05UnJugadorEnSalidaTerminaEnLaPosicionCorrectaAlAvanzar25LugaresYRetroceder43(){
+		Jugador jugador = new Jugador();
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores.add(jugador);
+		Tablero tablero = new Tablero(jugadores);
+		tablero.mover(jugador,25);
+		tablero.mover(jugador,-43);
+		Assert.assertEquals(2, tablero.getPosicion(jugador));
+	}
+}
