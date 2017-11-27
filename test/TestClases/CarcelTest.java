955f29ef2c77e5cd6d30925c653f00bbf05ee878
Pablo Massuh <pablomassh@gmail.com>
2017-11-27 17:12:57 -0300

    Bug fix en carcerl test

--- test/TestClases/CarcelTest.java
+++ test/TestClases/CarcelTest.java
@@ -67,7 +67,6 @@ public class CarcelTest {
 		jugador.aumentarTurnosEnCarcel();
 		jugador.aumentarTurnosEnCarcel();
 		jugador.aumentarTurnosEnCarcel();
-		jugador.pagarFianzaDeCarcel();
 		Assert.assertTrue( jugador.puedeHacerAcciones());
 	}
 
