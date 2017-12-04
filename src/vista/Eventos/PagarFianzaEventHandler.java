bac03c69c8dcb245025c9f99038d02d102720ef4
Aaron Sanjines <alivetheoldschool@hotmail.com>
2017-12-04 10:26:36 -0300

    Arreglo en mensaje de carcel

--- src/vista/Eventos/PagarFianzaEventHandler.java
+++ src/vista/Eventos/PagarFianzaEventHandler.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Region;
 
 public class PagarFianzaEventHandler implements EventHandler<ActionEvent> {
 	private Jugador jugador;
@@ -20,11 +21,13 @@ public class PagarFianzaEventHandler implements EventHandler<ActionEvent> {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Fianza pagada exitosamente!");
 			alert.setContentText("Usted ha pagado la fianza con exito, se le desconto 45000");
+			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);			
 			alert.show();
 		}else {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("¡Usted no esta en condiciones de pagar la fianza!");
 			alert.setContentText("Usted no puede pagar la fianza porque no paso mas de un turno o porque no tiene la plata suficiente");
+			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
 		}
 		
